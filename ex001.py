492b5421a22781dc922eb57bc0fdb8297184922a
audreyemmely <aerv@ic.ufal.br>
2019-10-25 21:27:17 -0300

    lê numero e mostra antecessor/sucessor

---
+++ ex001.py
@@ -0,0 +1,3 @@
+#Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor
+numero = int(input('Digite um número:\n'))
+print("Analisando o valor {}, seu antecessor é {} e seu sucessor é {}".format(numero, numero-1, numero+1))
