672c68b32343bb3d3242934305a9e8d5049fa585
audreyemmely <aerv@ic.ufal.br>
2019-10-27 09:29:43 -0300

    primeira e última ocorrência de um caractere

---
+++ fundamentos/ex018.py
@@ -0,0 +1,9 @@
+'''
+Faça um programa que leia uma frase pelo teclado e mostre quantas vezes aparece a letra "A", 
+em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.
+'''
+frase = str(input('Digite uma frase:\n')).strip().upper()
+print('A letra "A" aparece {} vezes na frase.'.format(frase.count('A')))
+print('A primeira letra "A" apareceu na posição {}'.format(frase.find('A')+1))
+print('A última letra "A" apareceu na posição {}'.format(frase.rfind('A')+1))
+
