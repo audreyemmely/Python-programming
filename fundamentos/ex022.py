7769221b029f9cccd9d78340474d95b7cfe83bd2
audreyemmely <aerv@ic.ufal.br>
2019-10-27 21:10:22 -0300

    mostrando se um número é par ou ímpar

---
+++ fundamentos/ex022.py
@@ -0,0 +1,9 @@
+'''
+Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
+'''
+
+numero = int(input('Digite um número:\n'))
+if numero%2==0:
+    print('Número par')
+else:
+    print('Número ímpar')
