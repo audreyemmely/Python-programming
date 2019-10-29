984b6aecdb677d2984ee1f54211a71c30abc812b
audreyemmely <aerv@ic.ufal.br>
2019-10-29 15:39:09 -0300

    calculando média e mostrando msg de acordo c nota

---
+++ estruturas de controle/ex005.py
@@ -0,0 +1,18 @@
+'''
+Crie um programa que leia duas notas de um aluno e calcule sua média, mostrando uma mensagem no final, 
+de acordo com a média atingida:
+- Média abaixo de 5.0: REPROVADO
+- Média entre 5.0 e 6.9: RECUPERAÇÃO
+- Média 7.0 ou superior: APROVADO
+'''
+nota1 = float(input('Digite a primeira nota do aluno:\n'))
+nota2 = float(input('Digite a segunda nota do aluno:\n'))
+
+media = (nota1+nota2)/2
+
+if media < 5:
+    print('Reprovado.')
+elif media >= 7:
+    print('Aprovado.')
+else:
+    print('Recuperação.')
