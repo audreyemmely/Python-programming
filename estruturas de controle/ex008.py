90e2fad770cf86e5368570c52a4f4ab03eed2a95
audreyemmely <aerv@ic.ufal.br>
2019-11-07 12:06:11 -0300

    calculando imc

--- estruturas de controle/ex008.py
+++ estruturas de controle/ex008.py
@@ -14,11 +14,11 @@ imc = peso/pow(altura, 2)
 
 if imc < 18.5:
     print('Abaixo do peso')
-elif imc >= 18.5 and imc < 25:
+elif 18.5 <= imc < 25:
     print('Peso ideal')
-elif imc >= 25 and imc < 30:
+elif 25 <= imc < 30:
     print('Sobrepeso')
-elif imc >= 30 and imc < 40:
+elif 30 <= imc < 40:
     print('Obesidade')
 else:
     print('Obesidade mórbida')
