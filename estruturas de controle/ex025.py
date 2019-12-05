346d83225a09b9222f3f5e374e84ed12d3fc5848
Audrey <aerv@ic.ufal.br>
2019-12-05 16:55:15 -0300

    calculando fatorial

---
+++ estruturas de controle/ex025.py
@@ -0,0 +1,19 @@
+'''
+Faça um programa que leia um número qualquer e mostre o seu fatorial.
+'''
+#from math import factorial
+#n = int(input('Digite um número para calcular seu fatorial: '))
+#f = factorial(n)
+#print('O fatorial de {} é {}.'.format(n, f))
+
+n = int(input('Digite um número para calcular seu fatorial: '))
+c = n
+f = 1
+print('{}! = '.format(n), end = '')
+while c > 0:
+    print('{}'.format(c), end='')
+    print(' x ' if c > 1 else ' = ', end='')
+    f *= c
+    c -= 1
+print('{}\n'.format(f))
+
