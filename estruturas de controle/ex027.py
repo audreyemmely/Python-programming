bbe812f7d52dcd42e314c93527358e58f68d1fa6
Audrey <aerv@ic.ufal.br>
2019-12-13 11:52:28 -0300

    PA com pausa

---
+++ estruturas de controle/ex027.py
@@ -0,0 +1,21 @@
+'''
+Melhore o EX026, perguntando para o usuário se ele quer mostrar mais alguns termos. 
+O programa encerrará quando ele disser que quer mostrar 0 termos.
+'''
+print('Gerador de PA')
+print('-='*10)
+primeiro = int(input('Primeiro termo: '))
+razão = int(input('Razão da PA: '))
+termo = primeiro
+cont = 1
+total = 0
+mais = 10
+while mais != 0:
+    total += mais
+    while cont <= total:
+        print('{} -> '.format(termo), end='')
+        termo += razão
+        cont+=1
+    print('PAUSA')
+    mais = int(input('Quantos termos você quer mostrar a mais? '))
+print('Progressão finalizada com {} termos mostrados'.format(total))
