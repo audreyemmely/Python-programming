5a2f488ab1ea72343bc4f9103d04ca487488e954
Audrey <aerv@ic.ufal.br>
2019-12-14 10:53:51 -0300

    utilizando break

---
+++ estruturas de controle/ex031.py
@@ -0,0 +1,14 @@
+'''
+Crie um programa que leia números inteiros pelo teclado.
+O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada. 
+No final, mostre quantos números foram digitados e qual foi a soma entre elas (desconsiderando o flag).
+'''
+soma = cont = 0
+
+while True:
+    num = int(input('Digite um número [999 para parar]: '))
+    if num == 999:
+        break
+    cont += 1
+    soma += num
+print('Foram digitados {} valores e a soma entre eles foi {}.'.format(cont, soma))
