d86785b90bbe38297ff15999dedc3c9968a15a5b
Audrey <aerv@ic.ufal.br>
2019-12-15 16:52:11 -0300

    pedindo número ao usuário e mostrando-o na tela

---
+++ ListaDeExercícios/EstruturaSequencial/ex2.py
@@ -0,0 +1,5 @@
+'''
+Faça um programa que peça um número e então mostre a mensagem "O número informado foi [número]".
+'''
+número = int(input('Digite um número: '))
+print('O número informado foi {}'.format(número))
