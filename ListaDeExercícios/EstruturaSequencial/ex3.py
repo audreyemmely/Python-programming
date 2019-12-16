8ee47847676ef2488881f24093d7de3b9415cc4e
Audrey <aerv@ic.ufal.br>
2019-12-16 08:29:21 -0300

    somando dois números

---
+++ ListaDeExercícios/EstruturaSequencial/ex3.py
@@ -0,0 +1,6 @@
+'''
+Faça um programa que peça dois números e imprima a soma.
+'''
+num1 = float(input('Digite o primeiro número: '))
+num2 = float(input('Digite o segundo número: '))
+print('{:.2f}'.format(num1+num2))
