e685ef70173a0e124f7303ccd5e24242687da7bb
Audrey <aerv@ic.ufal.br>
2019-12-19 10:41:35 -0300

    dizendo se valor é positivo ou negativo

---
+++ ListaDeExercícios/EstruturaDeDecisão/ex02.py
@@ -0,0 +1,8 @@
+'''
+Faça um programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+'''
+valor = float(input('Digite um valor: '))
+if valor >= 0:
+    print('Positivo')
+else:
+    print('Negativo')
